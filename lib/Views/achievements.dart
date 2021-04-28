97539703f94be61e56555ad5afd37e893373ae1f
Lukas Streinu <lukas.streinu@htwg-konstanz.de>
2021-04-28 17:54:12 +0200

    added 3 achievements

--- lib/Views/achievements.dart
+++ lib/Views/achievements.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:water_tracker/Persistence/SharedPref.dart';
 import 'package:water_tracker/icons/my_flutter_app_icons.dart';
+import 'package:water_tracker/Views/home.dart';
 
-class Achievements  extends StatefulWidget {
+class Achievements extends StatefulWidget {
   Achievements({Key key, this.title}) : super(key: key);
 
   final String title;
@@ -12,7 +13,6 @@ class Achievements  extends StatefulWidget {
 }
 
 class _AchievementsState extends State<Achievements> {
-
   @override
   void initState() {
     super.initState();
@@ -34,138 +34,163 @@ class _AchievementsState extends State<Achievements> {
     });
   }
 
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: Colors.transparent,
-
       body: Align(
-        alignment: Alignment(0.60,-0.80),
-
-      child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-
-
-          children: <Widget>[
-       AchievementCircle(
-
-          color: Color.fromRGBO(255, 255, 255, 1.0),
-          current: _totalWaterAmount.toDouble()/1000,
-          max: 10,
-          subtitle: "Total Water"),
-
-            AchievementCircle(
-
-                color: Color.fromRGBO(255, 255, 255, 1.0),
-                current: _currentCupCounter.toDouble(),
-                max: 100,
-                subtitle: "Total Cups"),
-
-            AchievementCircle(
-
-                color: Color.fromRGBO(255, 255, 255, 1.0),
-                current: 4,
-                max: 10,
-                subtitle: "Streak"),
-
-
-
-
-
-
-
-      ]
-    ),
-    ),
+        alignment: Alignment(0.60, -0.80),
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              AchievementCircle(
+                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                  colorBoarder: Color.fromARGB(153, 51, 0, 2),
+                  isCurrentInt: false,
+                  currentDouble: _totalWaterAmount / 1000,
+                  max: 10,
+                  unit: "Liter",
+                  subtitle: "Total Water"),
+              AchievementCircle(
+                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                  colorBoarder: Color.fromARGB(136, 83, 82, 82),
+                  isCurrentInt: true,
+                  currentInt: _currentCupCounter.round(),
+                  max: 100,
+                  unit: "Cups",
+                  subtitle: "Total Cups"),
+              AchievementCircle(
+                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                  colorBoarder: Color.fromARGB(153, 255, 223, 17),
+                  isCurrentInt: true,
+                  currentInt: 260,
+                  max: 360,
+                  unit: "Days",
+                  subtitle: "Streak"),
+            ]),
+      ),
     );
   }
 }
 
-
-
-
-
-
-
 class AchievementCircle extends StatelessWidget {
-  final Color color;
-  final String  subtitle;
-  double current,max;
+  Color color;
+  Color colorBoarder;
+  final String subtitle;
+  int currentInt, max;
+  double currentDouble;
+  bool isCurrentInt;
+  String unit;
 
-
-  AchievementCircle({this.color, this.current, this.max, this.subtitle});
+  AchievementCircle(
+      {this.color,
+      this.currentInt,
+      this.max,
+      this.subtitle,
+      this.unit,
+      this.isCurrentInt,
+      this.currentDouble,
+      this.colorBoarder});
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-
-      children: <Widget>[
-
-        Container(
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            color: this.color,
-            border: Border.all(
-              color: Color.fromRGBO(231, 241, 248, 1.0),
-              width: 3.0,
-            ),
-          ),
-          padding: EdgeInsets.all(6.0),
-          transform: Matrix4.translationValues(0.0, -30.0, 0.0),
-          height: 110.0,
-          width: 120.0,
-          alignment: Alignment.center,
-          child: Container(
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              color: this.color,
-              border: Border.all(
-                color: (this.color != Color.fromRGBO(255, 255, 255, 1.0))
-                    ? Colors.white
-                    : Colors.blue[100],
-                width: 3.0,
-              ),
-            ),
-            alignment: Alignment.center,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  this.current.round().toString() + "/" + this.max.round().toString(),
-                  style: TextStyle(
-                    color: (this.color != Color.fromRGBO(255, 255, 255, 1.0))
-                        ? Colors.white
-                        : Colors.blue[100],
-                    fontWeight: FontWeight.w900,
-                    fontSize: 20.0,
+    return Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Container(height: 40.0),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: this.color,
+                  border: Border.all(
+                    color: colorBoarder,
+                    width: 9.0,
                   ),
                 ),
-
-                    Text(
-                      this.subtitle,
-                      style: TextStyle(
-                        color: (this.color != Color.fromRGBO(255, 255, 255, 1.0))
-                            ? Colors.black54
-                            : Colors.blue[100],
-                        fontSize: 14.0,
-                      ),
+                padding: EdgeInsets.all(6.0),
+                transform: Matrix4.translationValues(0.0, -30.0, 0.0),
+                height: 110.0,
+                width: 120.0,
+                alignment: Alignment.center,
+                child: Container(
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: this.color,
+                    border: Border.all(
+                      color: (this.color != Color.fromRGBO(255, 255, 255, 1.0))
+                          ? Colors.white
+                          : Colors.blue[100],
+                      width: 2.0,
                     ),
-              ],
-            ),
+                  ),
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      isCurrentInt == true
+                          ? Text(
+                              this.currentInt.toString() +
+                                  "/" +
+                                  this.max.round().toString(),
+                              style: TextStyle(
+                                color: (this.color !=
+                                        Color.fromRGBO(255, 255, 255, 1.0))
+                                    ? Colors.white
+                                    : Colors.blue[100],
+                                fontWeight: FontWeight.w900,
+                                fontSize: 18.0,
+                              ),
+                            )
+                          : Text(
+                              this.currentDouble.toString() +
+                                  "/" +
+                                  this.max.round().toString(),
+                              style: TextStyle(
+                                color: (this.color !=
+                                        Color.fromRGBO(255, 255, 255, 1.0))
+                                    ? Colors.white
+                                    : Colors.blue[100],
+                                fontWeight: FontWeight.w900,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                      Text(
+                        this.unit,
+                        style: TextStyle(
+                          color:
+                              (this.color != Color.fromRGBO(255, 255, 255, 1.0))
+                                  ? Colors.black54
+                                  : Colors.blue[100],
+                          fontSize: 14.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
-      ],
-    );
+          Container(
+              alignment: Alignment.center,
+              child: Text(
+                this.subtitle + " ",
+                style: TextStyle(
+                  color: (this.color != Color.fromRGBO(231, 0, 0, 1.0))
+                      ? Colors.black54
+                      : Colors.blue[100],
+                  fontSize: 18.0,
+                ),
+              )),
+        ]);
   }
 }
 
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) {
+@override
+bool shouldRepaint(CustomPainter oldDelegate) {
   return false;
-  }
+}
