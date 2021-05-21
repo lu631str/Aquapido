1604b341371719f0c34b32f9cfe78a862f9d1838
Roland Burke <Roland.Burke@gmx.de>
2021-05-21 18:21:07 +0200

    added screenshot testing, sonarqube and jenkins

---
+++ test/lib/example_test.dart
@@ -0,0 +1,17 @@
+import 'package:golden_toolkit/golden_toolkit.dart';
+import 'package:water_tracker/Views/achievements.dart';
+import 'package:water_tracker/Views/statistics.dart';
+
+import 'Devices.dart';
+
+void main() {
+  testGoldens('testGoldens', (tester) async {
+    final builder = DeviceBuilder()
+      ..overrideDevicesForAllScenarios(devices: 
+        devicesWithDifferentTextScales,
+      )
+      ..addScenario(widget: Statistics());
+    await tester.pumpDeviceBuilder(builder);
+    await screenMatchesGolden(tester, 'initial');
+  });
+}
