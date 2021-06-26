2021c677b89b9d78a69a3522b8792068cb811114
Lukas Streinu <lukas.streinu@htwg-konstanz.de>
2021-06-26 13:56:23 +0200

    changed format change design

--- lib/Widgets/statistics/Calendar.dart
+++ lib/Widgets/statistics/Calendar.dart
@@ -1,9 +1,7 @@
-
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:water_tracker/Models/SettingsModel.dart';
-
 
 class Calendar extends StatefulWidget {
   //  final List<DateTime> dailyGoalReachedDates;
@@ -19,7 +17,7 @@ class CalendarState extends State<Calendar> {
   CalendarFormat _calendarFormat = CalendarFormat.twoWeeks;
   DateTime _focusedDay = DateTime.now();
   DateTime _selectedDay;
-  List<DateTime> dailyGoalReachDates ;
+  List<DateTime> dailyGoalReachDates;
 
   List<Color> gradientColors = [
     const Color(0xffed882f),
@@ -31,79 +29,92 @@ class CalendarState extends State<Calendar> {
     const Color(0xff09ff16),
   ];
 
-
-
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: EdgeInsets.only(top: 24, right: 8, bottom: 10, left: 7),
-        child: TableCalendar(
-         // rowHeight: MediaQuery.of(context).size.height / 24,
-            locale:'en_US' ,
-            firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: _focusedDay,
-          calendarFormat: _calendarFormat,
-          selectedDayPredicate: (day) {
-            // Use `selectedDayPredicate` to determine which day is currently selected.
-            // If this returns true, then `day` will be marked as selected.
-
-            // Using `isSameDay` is recommended to disregard
-            // the time-part of compared DateTime objects.
-            return isSameDay(_selectedDay, day);
-          },
-          onDaySelected: (selectedDay, focusedDay) {
-            if (!isSameDay(_selectedDay, selectedDay)) {
-              // Call `setState()` when updating the selected day
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-                Provider.of<SettingsModel>(context, listen: false)
-                    .setSelectedDate(_selectedDay);
-              });
-            }
-          },
-          onFormatChanged: (format) {
-            if (_calendarFormat != format) {
-              // Call `setState()` when updating calendar format
-              setState(() {
-                _calendarFormat = format;
-              });
-            }
-          },
-          onPageChanged: (focusedDay) {
-            // No need to call `setState()` here
-            _focusedDay = focusedDay;
-          },
-          eventLoader: (day) {
-             return [DateTime.utc(2010, 10, 16)];
-          },
-
-          calendarStyle: CalendarStyle(
-            selectedDecoration:   BoxDecoration(gradient: LinearGradient(
-          begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-            colors: [
-              Theme.of(context).primaryColor,
-              Theme.of(context).accentColor
-            ],
-          ), shape: BoxShape.circle),
-              todayDecoration: BoxDecoration(gradient: LinearGradient(
-                begin: Alignment.topRight,
-                end: Alignment.bottomLeft,
-                colors: [
-                   Theme.of(context).primaryColor.withOpacity(0.4),
-                   Theme.of(context).accentColor.withOpacity(0.4),
-                ],
-              ), shape: BoxShape.circle),
-            markerDecoration: BoxDecoration(gradient: LinearGradient(colors: gradientColorsGreen), shape: BoxShape.circle,  ),
-              markersAutoAligned: true,
-            markerSizeScale:0.4
+      padding: EdgeInsets.only(top: 24, right: 8, bottom: 10, left: 7),
+      child: TableCalendar(
+        // rowHeight: MediaQuery.of(context).size.height / 24,
+        locale: 'en_US',
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 3, 14),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
 
 
+
+
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+              Provider.of<SettingsModel>(context, listen: false)
+                  .setSelectedDate(_selectedDay);
+            });
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
+        // eventLoader: (day) {
+        //   return [DateTime.utc(2010, 10, 16)];
+        // },
+        headerStyle: HeaderStyle(
+          formatButtonDecoration:BoxDecoration(
+           color:Theme.of(context).primaryColor,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(40),
           ),
-
+          formatButtonTextStyle: TextStyle(color: Colors.white)
         ),
+
+        calendarStyle: CalendarStyle(
+            selectedDecoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Theme.of(context).primaryColor,
+                    Theme.of(context).accentColor
+                  ],
+                ),
+                shape: BoxShape.circle),
+            todayDecoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Theme.of(context).primaryColor.withOpacity(0.4),
+                    Theme.of(context).accentColor.withOpacity(0.4),
+                  ],
+                ),
+                shape: BoxShape.circle),
+            markerDecoration: BoxDecoration(
+              gradient: LinearGradient(colors: gradientColorsGreen),
+              shape: BoxShape.circle,
+            ),
+            markersAutoAligned: true,
+            markerSizeScale: 0.4),
+      ),
     );
   }
 }
